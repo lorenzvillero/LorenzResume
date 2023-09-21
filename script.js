4a57493e2cb756da4fc92413c49b633b0fe1a932
lorenzvillero <145584913+lorenzvillero@users.noreply.github.com>
2023-09-21 11:52:55 +0800

    added bootstrap ❤

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("loading-spinner").style.display = "block";
+
+// Simulate a delay (replace this with your actual loading logic)
+setTimeout(function () {
+    // Hide the spinner
+    document.getElementById("loading-spinner").style.display = "none";
+    // Add your content here
+}, 1000); 
